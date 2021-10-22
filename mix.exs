0f9c8260e1f843607fbf9fbec532cacadcceeecf
Greg Gradwell <greghgradwell@live.com>
2021-10-22 06:00:03 -0700

    Incremented version.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule RealflightIntegration.MixProject do
   use Mix.Project
 
-  @version "0.1.1"
+  @version "0.1.2"
   @source_url "https://github.com/copperpunk-elixir/realflight-integration"
 
   def project do
